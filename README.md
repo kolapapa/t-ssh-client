74b02322145b8b32432cce2158e2824d57fc7ae4
kolapapa <kolapapa2021@gmail.com>
2022-07-06 20:39:03 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # t-ssh-client
-`thrussh` based `ssh client`
+Rust async `ssh client` wrapped by [thrussh](https://pijul.org/thrussh).
+
+## Example
+```rust
+use t_ssh_client::Session;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sess = Session::connect("192.168.62.1:22").await?;
+    match sess.auth("username", "password").await? {
+        true => {
+            let output = sess.execute("df -h").await?;
+            if output.success() {
+                println!("{}", output.stdout());
+            } else {
+                println!("{}", output.stderr());
+            }
+        }
+        false => {
+            println!("login failed!");
+        }
+    }
+    sess.close().await?;
+    Ok(())
+}
+```
--- examples/sample.rs
+++ examples/sample.rs
@@ -2,10 +2,10 @@ use t_ssh_client::Session;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut ssh = Session::connect("192.168.62.1:22").await?;
-    match ssh.auth("username", "password").await? {
+    let mut sess = Session::connect("192.168.62.1:22").await?;
+    match sess.auth("username", "password").await? {
         true => {
-            let output = ssh.execute("df -h").await?;
+            let output = sess.execute("df -h").await?;
             if output.success() {
                 println!("{}", output.stdout());
             } else {
@@ -16,5 +16,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("login failed!");
         }
     }
+    sess.close().await?;
     Ok(())
 }
